bbf0d4103b433dfa46a51db681a0a6582d1540cd
Vanshika <vanshikajoshi072@gmail.com>
2026-09-23 20:45:40 +0530

    check pallindrome

--- extraction.py
+++ extraction.py
@@ -1,4 +1,5 @@
 # count number of digits-
+"""
 n = int(input("Enter a number: "))
 count = 0
 while n > 0:
@@ -6,3 +7,17 @@ while n > 0:
     n = n // 10
 print("The number of digits are: ", count)
 # T.C=O(log10(n))
+"""
+
+# check pallindrome-
+num = int(input("Enter a number: "))
+temp_num = num
+new = 0
+while temp_num > 0:
+    new = new * 10 + temp_num % 10
+    temp_num = temp_num // 10
+if new == num:
+    print("pallindrome")
+else:
+    print("Not pallindrome")
+# T.C-O(log10(n));
